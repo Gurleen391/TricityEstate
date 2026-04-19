af910b0d27f71b222260cd0a1e900b145c6a4a74
Gurleen Kaur Dhaliwal <dhaliwalgurleen735@email.com>
2026-04-20 00:34:43 +0530

    fix map marker label

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -187,27 +187,21 @@ export default function Listing() {
             </div>
 
             {/* MAP */}
-            {/* MAP */}
-<div className='w-full md:w-[400px] h-[300px] sticky top-24'>
-  <MapContainer
-    center={position}
-    zoom={13}
-    className='h-full w-full rounded-lg shadow-lg'
-  >
-    <TileLayer
-      attribution='&copy; OpenStreetMap contributors'
-      url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    />
-
-    {/* ✅ FIXED MARKER */}
-    <Marker position={position}>
-      <Popup closeButton={false}>
-        {listing.address}
-      </Popup>
-    </Marker>
-
-  </MapContainer>
-</div>
+            <div className='w-full md:w-[400px] h-[300px] sticky top-24'>
+              <MapContainer
+                center={position}
+                zoom={13}
+                className='h-full w-full rounded-lg shadow-lg'
+              >
+                <TileLayer
+                  attribution='&copy; OpenStreetMap contributors'
+                  url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                />
+                <Marker position={position}>
+                  <Popup>{listing.address}</Popup>
+                </Marker>
+              </MapContainer>
+            </div>
           </div>
 
           {/* CONTACT MODAL */}
